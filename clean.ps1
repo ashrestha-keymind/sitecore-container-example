eea602b67af4c5fcfdada445ad7c3d407fd6cc51
Amrit Shretha <amrit_sth@hotmail.com>
2024-06-01 20:42:59 +0000

    SPE tool and Sitecore Docker Tool

--- clean.ps1
+++ clean.ps1
@@ -5,16 +5,17 @@ docker compose down
 Get-ChildItem -Path ".\*-data" -Directory | ForEach-Object {
     $dataPath = $_.FullName
 
-    Get-ChildItem -Path $dataPath -Exclude "*.md" -Recurse | Remove-Item -Force -Recurse -Verbose
+    Get-ChildItem -Path $dataPath -Exclude .gitkeep,*.md -Recurse | Remove-Item -Force -Recurse -Verbose
+}
+
+# Delete artifacts
+Get-ChildItem -Path ".\docker\deploy" -Directory | ForEach-Object {
+    $deployPath = $_.FullName
+
+    Get-ChildItem -Path $deployPath -Exclude .gitkeep,*.md -Recurse | Remove-Item -Force -Recurse -Verbose
 }
 
 # Remove any networks
 docker network prune -f
-
-# Get-ChildItem -Path ".\deploy" -Directory | ForEach-Object {
-#     $deployPath = $_.FullName
-
-#     Get-ChildItem -Path $deployPath -Exclude ".gitkeep" -Recurse | Remove-Item -Force -Recurse -Verbose
-# }
 
 Write-Host "Clean up completed."
